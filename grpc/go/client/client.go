3303d34b52a297c3990eaec7b94989cb147bc5aa
panjiajun <1215007121@qq.com>
2023-07-07 17:15:48 +0800

    grpc demo 错误处理扩展

--- grpc/go/client/client.go
+++ grpc/go/client/client.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"log"
-	// "time"
 
 	pb "grpcexample/pb"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
@@ -35,9 +37,28 @@ func main() {
 	}
 	log.Printf("Greeting: %v", r.GetFieldMask())
 
-
 	_, err = c.Foo(ctx, &pb.FooRequest{})
 	if err != nil {
-		log.Printf("could not greet: %v", err)
+		st, ok := status.FromError(err)
+		if !ok {
+			log.Println(err)
+			return
+		}
+
+		switch st.Code() {
+		case codes.InvalidArgument:
+			for _, d := range st.Details() {
+				switch info := d.(type) {
+				case *errdetails.ErrorInfo:
+					//info.Reason
+					//info.Metadata
+					log.Printf("Request Field Invalid: %s", info)
+				default:
+					log.Printf("Unexpected error type: %s", info)
+				}
+			}
+		default:
+			log.Printf("Unhandled error : %s ", st.String())
+		}
 	}
 }
